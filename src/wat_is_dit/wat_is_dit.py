5d3ecf287c98c99e4618ee75f09d35997e56a217
nick <nickevers1@hotmail.com>
2020-09-24 15:48:25 +0200

    add basic search function

--- src/wat_is_dit/wat_is_dit.py
+++ src/wat_is_dit/wat_is_dit.py
@@ -5,6 +5,12 @@ from itertools import islice
 
 
 wat_is_dit = "D:/Downloads/watisdit.txt"
+highest = 0
+lowest = 999
+quality_score = {}
+seq_id = ""
+seq = ""
+quality = ""
 
 
 def calc_highest_lowest(_score, highest_score, lowest_score):
@@ -13,44 +19,49 @@ def calc_highest_lowest(_score, highest_score, lowest_score):
     return highest_score, lowest_score
 
 
-highest = 0
-lowest = 999
-quality_score = {}
-seq_id = ""
-seq = ""
-quality = ""
-with open(wat_is_dit, 'r') as file:
-    i = 0
-    # count = 0
-    for line in file:
-        if i == 0:
-            seq_id = line
-            i += 1
-        elif i == 1:
-            # seq = line
-            i += 1
-        elif i == 2:
-            i += 1
-        elif i == 3:
-            quality = line
-            score = py.mean([ord(ch) for ch in quality])
-            highest, lowest = calc_highest_lowest(score, highest, lowest)
-            quality_score[seq_id] = score
-            i = 0
-        # count += 1
-        # Debug amount of lines to read from file
-        # if count >= 8000000:
-        #     break
+def calc_scores():
+    with open(wat_is_dit, 'r') as file:
+        i = 0
+        count = 0
+        for line in file:
+            if i == 0:
+                seq_id = line
+                i += 1
+            elif i == 1:
+                # seq = line
+                i += 1
+            elif i == 2:
+                i += 1
+            elif i == 3:
+                quality = line
+                score = py.mean([ord(ch) for ch in quality])
+                highest, lowest = calc_highest_lowest(score, highest, lowest)
+                quality_score[seq_id] = score
+                i = 0
+            count += 1
+            # Debug amount of lines to read from file
+            if count >= 8000000:
+                break
 
-
-print(f"Highest score is: {highest}")
-print(f"Lowest score is: {lowest}")
+    print(f"Highest score is: {highest}")
+    print(f"Lowest score is: {lowest}")
 
 
 # 2)Retourneer een sequentie op basis van een sequentie ID.
+
+def search_seq(seq_id):
+    with open(wat_is_dit, 'r') as file:
+        for line in file:
+            if line[0] == "@":
+                if line[0:18] == seq_id:
+                    return file.readline()
+
+
+print(search_seq("@10031000100060826"))
+
+
 # 5)Filter het bestand op basis van een PhredScore (Threshold>9, of hoger).
 # 6)Sorteer het bestand op basis van PhredScore.
-# 7)Plotdedistributie van alle gevonden PhredScores in een histogram.
 
 plt.hist(quality_score.values(), bins='auto')  # arguments are passed to np.histogram
 plt.title("Score %")
